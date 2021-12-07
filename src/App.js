e6edc51bc21f0e3e5387d7a6a7d2f275a9f29433
Emmy-Iraturinze <iraturinzeemmanuel@gmail.com>
2021-12-07 16:22:21 +0200

    Grab toke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+import Keycloak from 'keycloak-js';
+
+
+
+const App = (props) => {
+
+
+    const [keycloak, setKeycloak] = useState(null);
+  	const [authenticated, setAuthenticated] = useState(false);
+
+
+
+	 
+    useEffect(() => { 
+
+   
+        
+
+    const keycloak = new Keycloak({
+        url: process.env.REACT_APP_KEYCLOAK_URL,
+        realm: process.env.REACT_APP_KEYCLOAK_REALM,
+        clientId : process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+    });
+    keycloak.init({ onLoad: 'login-required' }).then(
+        Authenticated => {
+        setKeycloak(keycloak);
+        setAuthenticated(Authenticated)  
+     
+    }
+    );
+    },[]);
+    
+
+
+
+    if(keycloak) {
+            if(authenticated){
+
+                // This is the method to update access token.                
+                keycloak.updateToken(300).then(function(refreshed) {
+                        if (refreshed) {
+                           // console.log('Token was successfully refreshed')  
+                        } else {
+                           console.log('Token is still valid');
+                        }
+                    }).catch(function() {
+                        keycloak.logout();
+                        //console.log('Failed to refresh token or the session has expired');
+                    });
+
+                return (    
+                            
+                            <div>
+                <h3>Congratulations, Now you have a valid token. Go ahead and refer to this code sample to create your own sms app</h3>
+                        </div>
+                    );
+        }else
+            return (
+            <div>404 Authorization failed</div>
+            );  
+    }else{
+        return (
+            <div>initialization...</div>
+          );
+    }
 }
 
 export default App;
